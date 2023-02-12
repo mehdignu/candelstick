07321f9fa8185fb64546e8894654afb6dfdf1c77
mehdi <mehdi.dridi@zalando.de>
2023-02-12 17:01:26 +0100

    persist quotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.slf4j.LoggerFactory
 import conf.DatabaseConf
 import service.InstrumentService
+import service.QuoteService
 
 private val logger = LoggerFactory.getLogger("MainKt")
 
 fun main() {
   logger.info("starting up")
   val instrumentService: InstrumentService = InstrumentService()
+  val quoteService: QuoteService = QuoteService()
 
   DatabaseConf.init()
 
@@ -26,6 +28,7 @@ fun main() {
 
   quoteStream.connect { event ->
     // TODO - implement
+    quoteService.handleQuoteUpdate(event)
     logger.info("Quote: {}", event)
   }
 
--- src/main/kotlin/model/Quote.kt
+++ src/main/kotlin/model/Quote.kt
@@ -5,12 +5,14 @@ import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
 data class QuoteEvent(val data: Quote)
-data class Quote(val isin: ISIN, val price: Price)
+data class Quote(val id: Int? = null, val isin: ISIN, val price: Price, val createdAt: LocalDateTime = LocalDateTime.now())
 
 object Quotes: IntIdTable() {
     val isin = varchar("isin", 100)
     val price = float("price")
     val createdAt = datetime("created_at").default(LocalDateTime.now())
+
+
 }
 
 
